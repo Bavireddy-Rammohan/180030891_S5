74327f17254aba61d32f38c21424ecf9583b061a
Bavireddy-Rammohan <83938820+Bavireddy-Rammohan@users.noreply.github.com>
2021-05-19 12:44:29 +0530

    Add files via upload

---
+++ WTN/Strings/String_8.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class String_8 {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String[] strs = str.split(".[\\*]+.");
+		StringBuffer sb = new StringBuffer();
+		for (String x : strs)
+			sb.append(x);
+		System.out.println(sb);
+	}
+}
