8b56f8dfaffc5d311910d32fac493f2b316cd320
Bavireddy-Rammohan <83938820+Bavireddy-Rammohan@users.noreply.github.com>
2021-05-21 10:49:45 +0530

    Add files via upload

---
+++ WTN/Packages Access control Using package/Program_1/Package_1.java
@@ -0,0 +1,10 @@
+package testpackage;
+import testpackage.Foundation;
+public class Package_1 {
+	public static void main(String args[])
+	{
+		Foundation foundation=new Foundation();
+		foundation.var4=5;
+		System.out.println(foundation.var4);
+	}
+}
