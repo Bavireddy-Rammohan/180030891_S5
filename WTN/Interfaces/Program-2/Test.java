b08ee6a366b61a22166626b81a1d9db6b280fbf6
Bavireddy-Rammohan <83938820+Bavireddy-Rammohan@users.noreply.github.com>
2021-05-22 12:02:51 +0530

    Add files via upload

---
+++ WTN/Interfaces/Program-2/Test.java
@@ -0,0 +1,11 @@
+package Interface_2;
+
+public class Test {
+	public static void main(String[] args) {
+		Playable veena = new Veena();
+		veena.play();
+		
+		Playable saxophone = new Saxophone();
+		saxophone.play();
+	}
+}
