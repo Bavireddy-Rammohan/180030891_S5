4a4fe8bb6b664431539b343ba5a84462c08b6169
Bavireddy-Rammohan <83938820+Bavireddy-Rammohan@users.noreply.github.com>
2021-05-24 11:08:27 +0530

    Add files via upload

---
+++ WTN/Exception Handling/P_1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class P_1 {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		try
+		{
+			int n=Integer.parseInt(str);
+			System.out.println("The square value is = "+(n*n));
+			System.out.println("The work has been done successfully");
+			
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("Entered input is not a valid format for an integer.");
+		}
+	}
+}
